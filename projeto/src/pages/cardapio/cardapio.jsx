f9c06fcc44456c12020162df5f2810abe4a0ea44
andre21cavalcante <andre.cavalcantejr21@gmail.com>
2022-05-04 18:42:10 -0300

    deletando

--- projeto/src/pages/cardapio/cardapio.jsx
+++ projeto/src/pages/cardapio/cardapio.jsx
@@ -13,6 +13,7 @@ import Camarao from "../../img/camarao.jpeg"
 import Espaguete from "../../img/espaguete.jpeg"
 import Panqueca from "../../img/pancake.jpeg"
 import Donuts from "../../img/Donuts.jpeg"
+import Button from "../../components/button/button.jsx"
 function Cardapio(){
     return(
 
@@ -22,62 +23,62 @@ function Cardapio(){
             <div>  
                 <Image src={Cord} />
                 <p className={styles.NomeComida}>Cordeiro</p>
-                <p className={styles.NomeComida}>Preço:R$ 100,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 100,00 <Button/></p>
             </div>
             <div>  
                 <Image src={frango}/>
                 <p className={styles.NomeComida}>NotChiken</p>
-                <p className={styles.NomeComida}>Preço:R$ 20,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 20,00 <Button/></p>
             </div>
             <div>
                 <Image src={Espaguete}/>
                 <p className={styles.NomeComida}>Espaguete</p>
-                <p className={styles.NomeComida}>Preço:R$ 30,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 30,00 <Button/></p>
             </div>
             <div>
                 <Image src={macarrao}/>
                 <p className={styles.NomeComida}>Macarrão Vegetariano</p>.
-                <p className={styles.NomeComida}>Preço:R$ 25,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 25,00 <Button/></p>
             </div>
             <div>
                 <Image src={taco}/>
                 <p className={styles.NomeComida}>Taco Vegetariano</p>
-                <p className={styles.NomeComida}>Preço:R$ 15,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 15,00 <Button/></p>
             </div>
             <div>
                 <Image src={salmao}/>
                 <p className={styles.NomeComida}>Moqueca de Salmón</p>
-                <p className={styles.NomeComida}>Preço:R$ 55,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 55,00 <Button/></p>
             </div>
             <div>
                 <Image src={Camarao}/>
                 <p className={styles.NomeComida}>Camarão com Torrada</p>
-                <p className={styles.NomeComida}>Preço:R$ 10,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 10,00 <Button/></p>
             </div>
             <div>
                 <Image src={mini}/>
                 <p className={styles.NomeComida}>MiniBurguer</p>
-                <p className={styles.NomeComida}>Preço:R$ 8,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 8,00 <Button/></p>
             </div>
             <div>
                 <Image src={Morango}/>
                 <p className={styles.NomeComida}>Iogurte de Morango</p>
-                <p className={styles.NomeComida}>Preço:R$ 5,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 5,00 <Button/></p>
             </div>
             <div>
                 <Image src={Oreo}/>
                 <p className={styles.NomeComida}>Copo da Felicidade de Oreo</p>
-                <p className={styles.NomeComida}>Preço:R$ 15,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 15,00 <Button/></p>
             </div>
             <div>
                 <Image src={Panqueca}/>
                 <p className={styles.NomeComida}>Panqueca Doce</p>
-                <p className={styles.NomeComida}>Preço:R$ 6,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 6,00 <Button/></p>
             </div>
             <div>
                 <Image src={Donuts}/>
                 <p className={styles.NomeComida}>Donuts</p>
-                <p className={styles.NomeComida}>Preço:R$ 5,00</p>
+                <p className={styles.NomeComida}>Preço:R$ 5,00 <Button/></p>
             </div>
         </Container>
     </div>
